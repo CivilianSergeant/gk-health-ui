6ed3d6e1846fd570deec4b9709b05dfce4bb8e65
momin <islam.shaiful7@gmail.com>
2021-03-23 13:06:43 +0600

    update prescription

--- src/views/Prescription/Prescription.vue
+++ src/views/Prescription/Prescription.vue
@@ -3,9 +3,10 @@
     <ContentBar :PageTitle="title"/>
     <b-alert v-model="isSuccess" variant="success">{{message}}</b-alert>
     <b-alert v-model="isError" variant="danger">{{message}}</b-alert>
+    <Loader :isBusy="isBusy"/>
     <b-form @submit.prevent="onSearch">
       <div class="row">
-        <div class="col-md-3">
+       <div class="col-md-3">
           <b-form-group
             id="input-group-patient-id"
             label="Invoice ID:"
@@ -27,63 +28,16 @@
             />
           </b-form-group>
         </div>
-        <div class="col-md-3 mt-4">
+        <div class="col-md-3 mt-4" style="margin-top:1.7rem">
           <!-- <b-button type="submit" variant="info">Search</b-button>  -->
           <b-button @click="onClearSearch" class="ml-1" variant="warning">Clear</b-button>
         </div>
-        <div class="col-md-12 text-secondary font-weight-bold">
-          <Loader :isBusy="isBusy"/>
-          <b-card v-if="patient!=null">
-            <div class="row">
-              <div class="col-md-6">
-                <b-card-title>Patient Info #{{patient.pid}}</b-card-title>
-                <div v-if="patient.id>0">
-                  <span>Patient Name: {{patient.fullName}}</span>
-                  <span v-if="patient.gender">, Sex: {{patient.gender}}</span>
-                  <span v-if="patient.age">, Age: {{patient.age}}</span>
-                </div>
-                <div>
-                  IS GB?:
-                  <Status :data="patient.registration.gb"/>
-                </div>
-                <div>
-                  Card Registered?:
-                  <Status :data="patient.registration && patient.registration.id"/>
-                </div>
-                <span
-                  v-if="patient.registration && patient.registration.id"
-                >Card Number: {{patient.registration.cardNumber}}</span>
-                <div v-if="patient.registration && patient.registration.members.length>0">
-                  Family Members
-                  <ul>
-                    <li
-                      v-for="(member,m) in form.cardRegistration.members"
-                      :key="m"
-                    >{{member.fullName}}</li>
-                  </ul>
-                </div>
-                <p v-if="patient.registration && patient.registration.validityDuration>0">
-                  Registration Valid for ({{patient.registration.validityDuration}}) Months From {{getDate(patient.registration.startDate)}}
-                  - {{getDate(patient.registration.expiredDate)}}
-                </p>
-              </div>
-              <div class="col-md-6">
-                <h5 class="mt-1">Invoice Detail</h5>
-                <p>Invoice No: {{invoice.invoiceNumber}}</p>
-                <ul class="nav">
-                  <li v-for="d in invoice.patientServiceDetails" :key="d.id">
-                    <p>{{d.service.name}} - {{d.payableAmount}} BDT</p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </b-card>
-        </div>
       </div>
+      <PatientInfo :invoice="invoice" :patient="patient" :form="form"></PatientInfo>
     </b-form>
-    <b-form @submit.prevent="onSubmitPrescription" @reset.prevent="onResetPrescription">
+    <b-form v-if="patient" @submit.prevent="onSubmitPrescription" @reset.prevent="onResetPrescription">
     <div class="row">
-      <div class="col-md-5">
+      <div class="col-md-4">
         <div class="row mt-1">
           <div class="col-md-12">
             <b-form-group
@@ -174,65 +128,12 @@
             </b-form-group>
           </div>
         </div>
-        <div class="row mt-1">
-          <div class="col-md-9">
-            <b-form-group
-              id="input-group-advice"
-              label="Advice:"
-              label-for="advice"
-              description="Advice for patient"
-            >
-              <b-textarea id="advice" v-model="prescription.advice" />
-            </b-form-group>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12">
-            <b-form-group
-              id="input-group-patient-id"
-              class="row"
-              label="Tests:"
-              label-for="test-id"
-              content-cols-md="12"
-              description="Select Test (optional)"
-            >
-              <div class="row">
-                <div class="col-md-10">
-                  <Autocomplete
-                    @choose-item="handleAutocomplete"
-                    :items="tests"
-                    label="name"
-                    rowId="serviceId"
-                    :disabled="!patient"
-                  />
-                </div>
-                <div class="col-md-2">
-                  <a class="ml-2 anchorBtn" pill variant="success" @click="addRequiredTest">
-                    <b-icon-plus-circle scale="1.25" class="t-bold"></b-icon-plus-circle>
-                  </a>
-                </div>
-              </div>
-            </b-form-group>
-          </div>
-
-          <div class="col-md-3 mt-4" v-if="service"></div>
-        </div>
-        <table class="table">
-              <tr v-for="(rt,r) in recommendedTests" :key="r">
-                <td style="width:180px">{{rt.service.name}}</td>
-                <td><b-form-input placeholder="Write any remark here" type="text" v-model="rt.remark"></b-form-input></td>
-                <!-- <td>
-                  <span v-for="(attr,a) in rt.attributes" :key="a" class="badge badge-info ml-2">{{attr.labTestAttribute.attributeName}} 
-                    <a class="removeAttrBtn anchorBtn" @click="removeAttribute(rt,a)"><b-icon-x scale="1.5" class="t-bold"></b-icon-x></a>
-                  </span>
-                </td> -->
-              </tr>
-            </table>
+     
       </div>
-      <div class="col-md-7 border-left">
+      <div class="col-md-8 border-left">
         <table class="table">
           <thead>
-            <tr>
+            <tr class="bg-light">
               <th>Medicine</th>
               <th>Rules</th>
               <th>Course Duration</th>
@@ -241,7 +142,7 @@
           </thead>
           <tbody>
             <tr v-for="(medicine,m) in recommendedMedicines" :key="m">
-              <td style="width:180px;">
+              <td>
                 <Autocomplete
                     @choose-item="handleMedicineAutocomplete"
                     :items="medicines"
@@ -251,17 +152,68 @@
                     :disabled="!patient"
                   />
               </td>
-              <td style="width:195px;">
+              <td>
                 <b-form-select id="r-rule" v-model="medicine.rule" 
                                         :options="feedingRules"></b-form-select>
               </td>
-              <td style="width:80px;"><b-form-input type="number" v-model="medicine.duration" ></b-form-input></td>
-              <td style="width:122px;"><b-form-select id="r-durationUnit" v-model="medicine.durationUnit" 
+              <td><b-form-input type="number" v-model="medicine.duration" ></b-form-input></td>
+              <td><b-form-select id="r-durationUnit" v-model="medicine.durationUnit" 
                                         :options="durationUnits"></b-form-select></td>
               
             </tr>
           </tbody>
         </table>
+        <!-- Advice and test section -->
+           <div class="row py-2 border-top bg-light mx-0">
+                <div class="col-md-4">
+                  <b-form-group
+                    id="input-group-advice"
+                    label="Advice:"
+                    label-for="advice"
+                    description="Advice for patient"
+                  >
+                    <b-textarea id="advice" v-model="prescription.advice" />
+                  </b-form-group>
+                </div>
+                <div class="col-md-8">
+                      <b-form-group
+                      id="input-group-patient-id"
+                      class="row"
+                      label="Tests:"
+                      label-for="test-id"
+                      content-cols-md="12"
+                      description="Select Test (optional)"
+                    >
+                    <div class="row">
+                      <div class="col-md-10">
+                        <Autocomplete
+                          @choose-item="handleAutocomplete"
+                          :items="tests"
+                          label="name"
+                          rowId="serviceId"
+                          :disabled="!patient"
+                        />
+                      </div>
+                      <div class="col-md-2">
+                        <a class="ml-2 anchorBtn" pill variant="success" @click="addRequiredTest">
+                          <b-icon-plus-circle scale="1.25" class="t-bold"></b-icon-plus-circle>
+                        </a>
+                      </div>
+                    </div>
+                  </b-form-group>
+                   <!-- <div class="col-md-3 mt-4" v-if="service"></div> -->
+                 <table class="table">
+                  <tr v-for="(rt,r) in recommendedTests" :key="r">
+                    <td style="width:180px">{{rt.service.name}}</td>
+                    <td><b-form-input placeholder="Write any remark here" class="w-75 d-inline-block" type="text" v-model="rt.remark"></b-form-input> 
+                   <b-icon-trash-fill @click="deleteTest(r)" variant="danger" class="d-inline-block cursor-pointer"></b-icon-trash-fill>
+                    </td>
+                  </tr>
+                </table>
+
+                </div>
+        </div>
+            <!-- end -->
         <uploder-ui accept=".png" 
         ref="Fileuploader"
         :disable="!patient" 
@@ -273,8 +225,11 @@
     <div class="row mb-3 pt-3 border-top">
         
       <div class="col-md-12 d-flex justify-content-end">
-          <b-button type="submit" variant="success">Submit</b-button>
-          <b-button type="reset" class="ml-4" variant="danger">Cancel</b-button>
+          <b-button type="submit" class=""  variant="info">Print</b-button>
+         <div>
+          <b-button type="submit" class="ml-2" variant="success">Submit</b-button>
+          <b-button type="reset" class="ml-2" variant="danger">Cancel</b-button>
+        </div>
       </div>
         
     </div>
@@ -293,4 +248,8 @@
 .removeAttrBtn:hover {
   background-color: rgba(255, 255, 255, 0.35);
 }
+.font-weight-bold{font-weight: 500 !important;}
+
+.table th{ border-top:0px}
+/* table .form-control{ height:20px;} */
 </style>
