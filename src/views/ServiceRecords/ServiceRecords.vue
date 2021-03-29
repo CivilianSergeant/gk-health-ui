9ed54e5f6143e2e0e787ad1a956adfd263246076
momin <islam.shaiful7@gmail.com>
2021-03-29 18:24:35 +0600

    updated service recoreds

--- src/views/ServiceRecords/ServiceRecords.vue
+++ src/views/ServiceRecords/ServiceRecords.vue
@@ -13,11 +13,40 @@
                 <b-button class="ml-2" variant="info">Search</b-button>
             </div>
         </div>
-        <b-table :fields="fields" :per-page="perPage" :current-page="currentPage" :items="serviceRecords">
+        <table class="table table-bordered">
+              <thead class="thead-light">
+                <tr>
+                    <th>Date</th>
+                    <th>Invoice No</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Receivable</th>
+                    <th>Paid</th>
+                </tr>
+              </thead>
+              <tbody>
+                   <tr v-for="(ps,i) in serviceRecords" :key="i">
+                    <td>{{ps.date}}</td>
+                    <td>{{ps.invoiceNumber}}</td>
+                    <td>{{ps.name}}</td>
+                    <td>{{ps.address}}</td>
+                    <td>{{ps.receivableAmount}}</td>
+                    <td>{{ps.paid}}</td>
+                </tr>
+              </tbody>
+              <tfoot>
+                  <tr>
+                      <th colspan="4">Total</th>
+                      <th>{{totalReceivable}}</th>
+                      <th>{{totalPaid}}</th>
+                  </tr>
+              </tfoot>
+        </table>
+        <!-- <b-table :fields="fields" :per-page="perPage" :current-page="currentPage" :items="serviceRecords">
               <template>
                     
             </template>
-        </b-table>
+        </b-table> -->
 
        
     </div>
@@ -35,16 +64,10 @@ export default {
         serviceRecords: {
         handler(serviceRecords){
             this.totalReceivable=0;
-        
+            this.totalPaid = 0;
             serviceRecords.map(r=>{
-            // if(this.registration.gb){
-            //   this.totalPayable+= r.currentGbCost
-            // }else{
-            //   this.totalPayable+= r.currentCost
-            // }
-            
-            
             this.totalReceivable+= r.receivableAmount;
+            this.totalPaid+= r.paid;
             });
             
         },
@@ -59,7 +82,8 @@ export default {
             perPage: 20,
             currentPage: 1,
             totalRows:0,
-            totalReceivable:0
+            totalReceivable:0,
+            totalPaid:0,
         }
         
     },
