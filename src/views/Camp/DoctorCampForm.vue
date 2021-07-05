415486a3f635d34e69345ed378982fb6a244acd1
saiful043 <islam.shaiful7@gmail.com>
2021-07-05 13:18:15 +0600

    camp ui design

--- src/views/Camp/DoctorCampForm.vue
+++ src/views/Camp/DoctorCampForm.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>test form</div>
+  <div>
+    <CCard>
+      <CCardHeader>
+        Add Doctor Camp
+        <router-link
+          to="/doctor-camps"
+          class="btn btn-primary btn-sm float-right"
+          >Doctor Camp List</router-link
+        >
+      </CCardHeader>
+      <CCardBody> </CCardBody>
+      <CCardFooter></CCardFooter>
+    </CCard>
+  </div>
 </template>
