79fe616fe6a49b8f5357155243c7a9273d90d824
saiful043 <islam.shaiful7@gmail.com>
2021-07-07 11:13:07 +0600

    update camp

--- src/views/Camp/DoctorCampForm.vue
+++ src/views/Camp/DoctorCampForm.vue
@@ -199,7 +199,6 @@ export default {
       villages: [],
       centers: [],
       raOffices: [],
-      thirdLevelCode: "",
       currentCenter: null,
       raCenters: [],
     };
--- src/views/Camp/DoctorCamps.vue
+++ src/views/Camp/DoctorCamps.vue
@@ -3,6 +3,82 @@
     <b-alert v-model="isSuccess" variant="success">{{ message }}</b-alert>
     <b-alert v-model="isError" variant="danger">{{ message }}</b-alert>
     <CCard>
+      <CCardHeader>Search</CCardHeader>
+      <b-form @submit.prevent="handleSearch" @reset.prevent="onClearSearch">
+        <CCardBody>
+          <div class="row">
+            <div class="col-md-3" v-if="showRaOfficeList">
+              <b-form-group id="center" label-for="camp-center">
+                <label> RA Office</label>
+                <b-form-select
+                  id="camp-center"
+                  :options="raOffices"
+                  v-model="search.raOffice"
+                  @change="handleChangeRaOffice"
+                ></b-form-select>
+              </b-form-group>
+            </div>
+
+            <div class="col-md-3">
+              <!-- <Loader class="mt-4" :isBusy="isBusy" /> -->
+              <!-- v-if="centers.length > 0" -->
+              <b-form-group id="center" label-for="camp-center">
+                <label> Center</label>
+                <b-form-select
+                  id="camp-center"
+                  :options="centers"
+                  v-model="search.center"
+                  type="text"
+                ></b-form-select>
+              </b-form-group>
+            </div>
+            <div class="col-md-2">
+              <b-form-group id="event-date" label-for="event-category">
+                <label> Event Category </label>
+                <b-form-select
+                  id="event-category"
+                  v-model="search.eventCategory"
+                  :options="eventCategories"
+                  type="text"
+                ></b-form-select>
+              </b-form-group>
+            </div>
+            <div class="col-md-2">
+              <b-form-group id="event-type" label-for="event-type">
+                <label> Event Type</label>
+                <b-form-select
+                  id="event-type"
+                  v-model="search.eventType"
+                  :options="eventTypes"
+                ></b-form-select>
+              </b-form-group>
+            </div>
+            <div class="col-md-2">
+              <b-form-group id="event-type" label-for="main-doctor">
+                <label> Doctor </label>
+                <b-form-select
+                  id="main-doctor"
+                  v-model="search.employeeId"
+                  :options="doctorList"
+                ></b-form-select>
+              </b-form-group>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-3">
+              <b-button type="submit" variant="info" class="mr-2"
+                >Search</b-button
+              >
+              <b-button type="reset" class="ml-1" variant="warning"
+                >Clear</b-button
+              >
+            </div>
+          </div>
+        </CCardBody>
+        <!-- <CCardFooter></CCardFooter> -->
+      </b-form>
+    </CCard>
+    <CCard>
       <CCardHeader>
         Doctor Camp List
         <router-link
@@ -41,6 +117,9 @@ import { LocationService, NavigationService } from "@/services";
 import { EventService, CenterService } from "@/services";
 export default {
   computed: {
+    showRaOfficeList() {
+      return this.$store.getters.center.officeTypeId == 1;
+    },
     rows() {
       return this.totalRows; //this.services.length
     },
@@ -59,6 +138,16 @@ export default {
   },
   data() {
     return {
+      eventCategories: [],
+      eventTypes: [
+        { value: null, text: "Select" },
+        { value: "camp", text: "Camp" },
+        { value: "visit", text: "Visit" },
+      ],
+      doctorList: [
+        { value: "", text: "Select" },
+        { value: "1", text: "Dr Farjana" },
+      ],
       fields: [
         { key: "sl", sortable: true },
         { key: "center", sortable: true },
@@ -69,6 +158,17 @@ export default {
         { key: "status", sortable: true },
         "action",
       ],
+      search: {
+        center: null,
+        eventDate: null,
+        eventCategory: null,
+        eventType: null,
+        employeeId: "",
+        village: null,
+        locationAddress: "",
+        note: "",
+        raOffice: null,
+      },
       perPage: 10,
       currentPage: 1,
       filter: null,
@@ -77,9 +177,66 @@ export default {
       totalRows: 0,
       sortBy: "",
       sortDesc: false,
+      raOffices: [],
+      centers: [],
+      currentCenter: null,
+      raCenters: [],
     };
   },
 
-  methods: {},
+  mounted() {
+    this.fetchEventCategories();
+    this.fetchRaOffices();
+  },
+
+  methods: {
+    fetchEventCategories() {
+      new EventService().getEventCategories().then((result) => {
+        this.eventCategories.push({ value: null, text: "Category" });
+        result.forEach((c) => {
+          this.eventCategories.push({
+            value: c.id,
+            text: c.name,
+          });
+        });
+      });
+    },
+    fetchCenters() {
+      this.$store.commit("start");
+      console.log(this.currentCenter);
+      const raOffice =
+        this.raCenters.length > 0
+          ? this.raCenters.filter((r) => r.id == this.currentCenter)[0]
+          : this.$store.getters.center;
+      console.log(raOffice);
+      new CenterService().getCentersByThirdLevel(raOffice).then((result) => {
+        //this.centers = result;
+        if (result.length > 0) {
+          this.centers.push({ value: null, text: "Select Center" });
+          result.forEach((hc) =>
+            this.centers.push({ value: hc.id, text: hc.name })
+          );
+        }
+        this.$store.commit("finish");
+      });
+    },
+    fetchRaOffices() {
+      new CenterService().getRaCenters().then((result) => {
+        this.raCenters = result;
+        this.raOffices.push({ value: null, text: "Select RA" });
+        result.forEach((c) => {
+          this.raOffices.push({
+            value: c.id,
+            text: c.name,
+          });
+        });
+      });
+    },
+    handleChangeRaOffice(val) {
+      this.currentCenter = val;
+      this.centers = [];
+      this.fetchCenters();
+    },
+  },
 };
 </script>
